1b52e8faccf9bb9f51d92f36988fae81a6ac6879
James Harrison <jamesebharrison@hotmail.com>
2021-09-23 16:18:35 +0100

    commented out previous code for new code

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 // If a name, is a coaches name, log "It's a coach"
 
-const name = "ollie";
-console.log("Its a coach")
+// const name = "ollie";
+// console.log("Its a coach")
 
-// If greeting, Bonjour, log "comment ca va"
+// // If greeting, Bonjour, log "comment ca va"
 
-const greeting = "Bonjour";
-if (greeting == "Bonjour") ;
-console.log("comment ca va") 
+// const greeting = "Bonjour";
+// if (greeting == "Bonjour") ;
+// console.log("comment ca va") 
 
-// If a score reaches 10, log "you won the game"
+// // If a score reaches 10, log "you won the game"
 
-const score = 10;
-if (score == 10);
-console.log("you won the game")
+// const score = 10;
+// if (score == 10);
+// console.log("you won the game")
 
-// If age is greater than 65, log "time to retire"
-var age = age > 65;
-if (age => 65);
-console.log("time to retire") 
+// // If age is greater than 65, log "time to retire"
+// var age = age > 65;
+// if (age => 65);
+// console.log("time to retire") 
 
-// If hasCovid, log "time to isolate", else "keep social distancing"
-// var hasCovid = console.log("time to isolate")
-// var doesntHaveCovid = console.log("keep social distancing")
-if (hasCovid = true) {
-    console.log('time to isolate')
-} else (hasCovid = false); {
-    console.log("keep social distancing")
-}
+// // If hasCovid, log "time to isolate", else "keep social distancing"
+// // var hasCovid = console.log("time to isolate")
+// // var doesntHaveCovid = console.log("keep social distancing")
+// if (hasCovid = true) {
+//     console.log('time to isolate')
+// } else (hasCovid = false); {
+//     console.log("keep social distancing")
+// }
 
-// If age, greater than 18, and is Britsh, log "you can vote in the next election"
-if (age => 18 && "is British"){
-    console.log("you can vote in the next election")
-}
+// // If age, greater than 18, and is Britsh, log "you can vote in the next election"
+// if (age => 18 && "is British"){
+//     console.log("you can vote in the next election")
+// }
 
 // if your hungry, log "eat cookie" else 'dont eat cookie
-if (hungry = true){
-    console.log("eat the cookie")
-} if  (hungry = false);
-    console.log("dont eat cookie")
+// if (hungry = true){
+//     console.log("eat the cookie")
+// } if  (hungry = false);
+//     console.log("dont eat cookie")
 
 
-if (taste = true){
-    console.log("Please enjoy")
-}
+// if (taste = true){
+//     console.log("Please enjoy")
+// }
 
